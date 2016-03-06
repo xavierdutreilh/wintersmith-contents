ab9de946dd312e18ff232e7def39890ba2be436e
Xavier Dutreilh <xavier@dutreilh.com>
2016-03-06 21:23:31 +0100

    Use ES6 features in test

--- test/contents_test.js
+++ test/contents_test.js
@@ -1,23 +1,23 @@
 'use strict';
 
-var fs = require('fs');
+const fs = require('fs');
 
 function read(filename) {
   return fs.readFileSync(filename, {'encoding': 'utf8'});
 }
 
 exports.contents = {
-  'filter': function(test) {
-    var actual = read('tmp/helpers/filter.html');
-    var expected = read('test/expected/helpers/filter.html');
+  'filter': (test) => {
+    const actual = read('tmp/helpers/filter.html');
+    const expected = read('test/expected/helpers/filter.html');
 
     test.equal(actual, expected, 'should filter all entries');
 
     test.done();
   },
-  'list': function(test) {
-    var actual = read('tmp/helpers/list.html');
-    var expected = read('test/expected/helpers/list.html');
+  'list': (test) => {
+    const actual = read('tmp/helpers/list.html');
+    const expected = read('test/expected/helpers/list.html');
 
     test.equal(actual, expected, 'should list all entries');
 
