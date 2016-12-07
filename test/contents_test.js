21d7db5657ab0bbbc2f05851b51bd9faf1a9a37f
Xavier Dutreilh <xavier@dutreilh.com>
2016-12-07 17:01:54 +0100

    Stop using path module

--- test/contents_test.js
+++ test/contents_test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 
 function read(filename) {
   return fs.readFileSync(filename, 'utf8');
@@ -9,16 +8,16 @@ function read(filename) {
 
 exports.contents = {
   'filter': (test) => {
-    const actual = read(path.join(__dirname, '..', 'tmp', 'filter.html'));
-    const expected = read(path.join(__dirname, 'expected', 'filter.html'));
+    const actual = read(`${__dirname}/../tmp/filter.html`);
+    const expected = read(`${__dirname}/expected/filter.html`);
 
     test.equal(actual, expected, 'should filter all entries');
 
     test.done();
   },
   'list': (test) => {
-    const actual = read(path.join(__dirname, '..', 'tmp', 'list.html'));
-    const expected = read(path.join(__dirname, 'expected', 'list.html'));
+    const actual = read(`${__dirname}/../tmp/list.html`);
+    const expected = read(`${__dirname}/expected/list.html`);
 
     test.equal(actual, expected, 'should list all entries');
 
