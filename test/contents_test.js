468338060da645fd839c9b0519577023813a9866
Xavier Dutreilh <xavier@dutreilh.com>
2017-03-01 21:03:42 +0100

    Switch to standard.js

--- test/contents_test.js
+++ test/contents_test.js
@@ -1,26 +1,24 @@
-'use strict';
+const fs = require('fs')
 
-const fs = require('fs');
-
-function read(filename) {
-  return fs.readFileSync(filename, 'utf8');
+function read (filename) {
+  return fs.readFileSync(filename, 'utf8')
 }
 
 exports.contents = {
   'filter': (test) => {
-    const actual = read(`${__dirname}/../tmp/filter.html`);
-    const expected = read(`${__dirname}/expected/filter.html`);
+    const actual = read(`${__dirname}/../tmp/filter.html`)
+    const expected = read(`${__dirname}/expected/filter.html`)
 
-    test.equal(actual, expected, 'should filter all entries');
+    test.equal(actual, expected, 'should filter all entries')
 
-    test.done();
+    test.done()
   },
   'list': (test) => {
-    const actual = read(`${__dirname}/../tmp/list.html`);
-    const expected = read(`${__dirname}/expected/list.html`);
+    const actual = read(`${__dirname}/../tmp/list.html`)
+    const expected = read(`${__dirname}/expected/list.html`)
 
-    test.equal(actual, expected, 'should list all entries');
+    test.equal(actual, expected, 'should list all entries')
 
-    test.done();
-  },
-};
+    test.done()
+  }
+}
